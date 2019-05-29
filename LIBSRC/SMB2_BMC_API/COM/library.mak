5ecacfb97d21e88ba2207b927261551053957147
KonradJPE <ktomasik@jpembedded.eu>
2019-05-29 11:50:38 +0200

    Add proper copyright, license and remove old history entries
    This modification is necessary to meet the:
    MDIS_ConfRule_0008
    MDIS_ConfRule_0012

--- LIBSRC/SMB2_BMC_API/COM/library.mak
+++ LIBSRC/SMB2_BMC_API/COM/library.mak
@@ -6,16 +6,21 @@
 #
 #    Description: Makefile descriptor file for SMB2_BMC_API lib
 #
-#---------------------------------[ History ]---------------------------------
-#
-#   $Log: library.mak,v $
-#   Revision 1.1  2015/08/14 15:35:22  awerner
-#   Initial Revision
-#
-#
 #-----------------------------------------------------------------------------
-#   (c) Copyright 2015 by MEN mikro elektronik GmbH, Nuernberg, Germany
+#   Copyright (c) 2015-2019, MEN Mikro Elektronik GmbH
 #*****************************************************************************
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 MAK_NAME=smb2_bmc_api
 MAK_LIBS=$(LIB_PREFIX)$(MEN_LIB_DIR)/smb2_api$(LIB_SUFFIX)  \
--- LIBSRC/SMB2_BMC_API/COM/smb2_bmc_api.c
+++ LIBSRC/SMB2_BMC_API/COM/smb2_bmc_api.c
@@ -9,23 +9,26 @@
  *       \brief  API functions to access the MEN BMC
  *
  *    \switches  -
- */
-/*-------------------------------[ History ]---------------------------------
  *
- * $Log: smb2_bmc_api.c,v $
- * Revision 1.3  2018/12/19 12:54:16  DPfeuffer
- * R:1. there is no working cluster implementation in any BMC
- *   2. SMB2BMC_StatusOutput_Set/SMB2BMC_GPO_Set/SMB2BMC_RTC_Set erroneous
- * M:1. removed cluster support
- *   2. fix SMB2BMC_StatusOutput_Set/SMB2BMC_GPO_Set/SMB2BMC_RTC_Set
- *
- * Revision 1.2  2015/08/14 17:53:31  awerner
- * R: Windows package does not compile due to some cast warnings.
- * M: Fixed warnings and added casts to the functions calls.
  *
  *---------------------------------------------------------------------------
- * (c) Copyright 2014 by MEN Mikro Elektronik GmbH, Nuernberg, Germany
+ * Copyright (c) 2014-2019, MEN Mikro Elektronik GmbH
  ****************************************************************************/
+
+ /*
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #include <stdio.h>
 #include <string.h>
