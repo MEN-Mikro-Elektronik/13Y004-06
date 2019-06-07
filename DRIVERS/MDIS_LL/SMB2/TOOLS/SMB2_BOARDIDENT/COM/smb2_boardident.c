9d8e9d138f5650555cd75a06523b3c503ff04ae3
KonradJPE <ktomasik@jpembedded.eu>
2019-06-07 10:51:20 +0200

    Minor syntax fix

--- DRIVERS/MDIS_LL/SMB2/TOOLS/SMB2_BOARDIDENT/COM/smb2_boardident.c
+++ DRIVERS/MDIS_LL/SMB2/TOOLS/SMB2_BOARDIDENT/COM/smb2_boardident.c
@@ -105,7 +105,7 @@ static void header(void)
 	printf("\n==========================="
 		   "\n===   SMB2_BOARDIDENT   ==="
 		   "\n===========================");
-    printf("\nCopyright (c) 2009-2019, MEN Mikro Elektronik GmbH\n%s\n\n", IdentString)
+    printf("\nCopyright (c) 2009-2019, MEN Mikro Elektronik GmbH\n%s\n\n", IdentString);
 }
 
 /********************************* usage ***********************************/
@@ -125,7 +125,7 @@ static void usage(void)
 		"    smb2_boardident smb2_1 -r \n"
 		"\n- dump data from specified SMB address: \n"
 		"    smb2_boardident smb2_1 0xac  \n");
-    printf("\nCopyright (c) 2009-2019, MEN Mikro Elektronik GmbH\n%s\n\n", IdentString)
+    printf("\nCopyright (c) 2009-2019, MEN Mikro Elektronik GmbH\n%s\n\n", IdentString);
 }
 
 /***************************************************************************/
