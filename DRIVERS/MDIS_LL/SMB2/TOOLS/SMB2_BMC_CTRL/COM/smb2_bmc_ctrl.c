f4abf1e743d606568dfa8f9de11960e5212af238
KonradJPE <ktomasik@jpembedded.eu>
2019-07-11 15:43:07 +0200

    Fix syntax error

--- DRIVERS/MDIS_LL/SMB2/TOOLS/SMB2_BMC_CTRL/COM/smb2_bmc_ctrl.c
+++ DRIVERS/MDIS_LL/SMB2/TOOLS/SMB2_BMC_CTRL/COM/smb2_bmc_ctrl.c
@@ -273,7 +273,7 @@ static void usage(void)
 		"Calling examples:\n"
 		"    Get usage for -b=[cmd]: smb2_bmc_ctrl smb2_1 -b=\n"
 		"    Get SMB2_BMC API rev  : smb2_bmc_ctrl smb2_1 -i\n\n");
-    printf("\nCopyright (c) 2014-2019, MEN Mikro Elektronik GmbH\n%s\n\n", IdentString)
+    printf("\nCopyright (c) 2014-2019, MEN Mikro Elektronik GmbH\n%s\n\n", IdentString);
 }
 
 /********************************* usage ************************************/
