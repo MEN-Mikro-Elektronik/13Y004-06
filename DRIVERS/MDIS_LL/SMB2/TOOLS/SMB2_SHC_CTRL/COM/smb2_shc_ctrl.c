11d343fc71d24e680367668c9e0d590012af160a
Rafael Bachmann <mediumendian@gmail.com>
2019-08-05 13:52:33 +0200

    fix bug where uint is used for celsius temperature

--- DRIVERS/MDIS_LL/SMB2/TOOLS/SMB2_SHC_CTRL/COM/smb2_shc_ctrl.c
+++ DRIVERS/MDIS_LL/SMB2/TOOLS/SMB2_SHC_CTRL/COM/smb2_shc_ctrl.c
@@ -373,7 +373,8 @@ static void print_ups(int32 ups_id)
 static void get_temperature()
 {
 	int err;
-	u_int16 tempK = 0, tempC = 0;
+        u_int16 tempK = 0;
+        int16 tempC = 0;
 
 	err = SMB2SHC_GetTemperature((u_int16*)&tempK);
 	if (err) {
